458eae1919642e7a1d5bc7769c22ddb193d9d34a
Stanley Klemme <st@nleyy-klemme.de>
2018-07-22 21:35:34 +1000

    added cout test for report command

--- test/test_report_command.cpp
+++ test/test_report_command.cpp
@@ -8,6 +8,9 @@
 #include <position.h>
 
 #include <boost/test/unit_test.hpp>
+#include <boost/test/output_test_stream.hpp>
+
+#include <iostream>
 
 using namespace ToyRobot;
 
@@ -36,6 +39,23 @@ struct ReportCommandFixture
     ReportCommand* reportCommand;
 };
 
+//this redirects std::cout to the boost::output_test_stream
+//https://www.boost.org/doc/libs/1_67_0/libs/test/doc/html/boost_test/testing_tools/output_stream_testing.html
+struct CoutRedirect
+{
+    CoutRedirect( std::streambuf* newBuffer ) :
+        oldBuffer( std::cout.rdbuf( newBuffer ) )
+    {
+    }
+
+    ~CoutRedirect()
+    {
+        std::cout.rdbuf( oldBuffer );
+    }
+private:
+    std::streambuf* oldBuffer;
+};
+
 BOOST_FIXTURE_TEST_SUITE( report_tests, ReportCommandFixture )
 
 BOOST_AUTO_TEST_CASE( report )
@@ -44,7 +64,16 @@ BOOST_AUTO_TEST_CASE( report )
     position.SetDirection( Direction( Direction::EAST ) );
     Coordinates expectedCoordinates( 3, 3 );
     Direction::DirectionEnum expectedDirection = Direction::EAST;
-    Position newPosition = reportCommand->Execute( position, grid );
+    std::string expectedOutput( "3,3,EAST\n" );
+    Position newPosition;
+
+    boost::test_tools::output_test_stream output;
+    {
+        CoutRedirect guard( output.rdbuf() );
+
+        newPosition = reportCommand->Execute( position, grid );
+    }
+    BOOST_CHECK( output.is_equal( expectedOutput ) );
     BOOST_CHECK_EQUAL( newPosition.GetCoordinates().GetX(), expectedCoordinates.GetX() );
     BOOST_CHECK_EQUAL( newPosition.GetCoordinates().GetY(), expectedCoordinates.GetY() );
     BOOST_CHECK_EQUAL( newPosition.GetDirection().GetDirection(), expectedDirection );
@@ -52,9 +81,6 @@ BOOST_AUTO_TEST_CASE( report )
     BOOST_CHECK_EQUAL( position.GetCoordinates().GetY(), expectedCoordinates.GetY() );
     BOOST_CHECK_EQUAL( position.GetDirection().GetDirection(), expectedDirection );
 }
-
-//report output test case?
-
 
 BOOST_AUTO_TEST_SUITE_END()
 
